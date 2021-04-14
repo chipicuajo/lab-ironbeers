1a6768fd3f23502de23d49094bdfdac493d1e041
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-14 17:31:48 +0200

    Mandatory done

--- app.js
+++ app.js
@@ -22,12 +22,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-// app.get('/beers', (req, res) => {
-//   res.render('beers');
-// });
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then((beersFromApi) =>{
+    res.render('beers',{beersFromApi})
+    })
+  
+  
+  .catch(error => console.log(error));  
+  
+});
 
-// app.get('/random-beer', (req, res) => {
-//   res.render('random-beer');
-// });
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then((beersFromApi)=>{
+    
+    res.render('random-beer', {beersFromApi});
+    console.log(beersFromApi)
+  })
+  .catch(error => console.log(error)); 
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
